051d58a910c1dbce7c397b8aaa6adefc45d5955e
ValeriiKutin <valerabulon@gmail.com.com>
2025-05-22 17:13:33 +0300

    validation upd2

--- app/design/frontend/VendorCustom/customtheme/Magento_Theme/web/js/my-custom-widget.js
+++ app/design/frontend/VendorCustom/customtheme/Magento_Theme/web/js/my-custom-widget.js
@@ -4,11 +4,13 @@ define(['jquery', 'validation','jquery-ui-modules/widget'], ($) => {
             this._on(this.element, {
                 'input .input-text': (event) => {
                     const $input = $(event.target)
-                    if(!$input.valid()) {
+                    console.log($input.valid())
+                    if($input.valid()) {
+                        $input.css('border-bottom', '3px solid green');
+
+                    }else {
                         $input.css('border-bottom', '3px solid red');
                         return this;
-                    }else {
-                        $input.css('border-bottom', '3px solid green');
                     }
                 }
             })
@@ -16,3 +18,14 @@ define(['jquery', 'validation','jquery-ui-modules/widget'], ($) => {
     });
     return $.perspective.customWidget;
 })
+/*ДЛЯ СЕБЕ
+* _create - це для того, щоб функція викликалась при ініціалізації віджета
+* this._on - це обробник подій(тут ми слухаємо подію input(при введені) на всіх інпутах де є клас .input-text)
+* в $input у нас сам елемет інпут(дом елемент)
+* ну і далі звичайна перевірка, якщо інпут валідний($input.valid() == true), тобто в файлі validation-mixin.js повертає на інпуті true, то цей інпут приймає стилі відповідні
+*
+*
+*
+*
+*
+* */
